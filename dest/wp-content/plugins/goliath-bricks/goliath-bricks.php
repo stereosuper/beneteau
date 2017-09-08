e9267e1a29a9201c57a488b041824565b54a5aea
Alain Diart <adiart@studio-goliath.com>
2017-09-08 15:56:09 +0200

    Ajout du plugin Goliath Bricks

---
+++ dest/wp-content/plugins/goliath-bricks/goliath-bricks.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Plugin Name: Goliath Simple Bricks
+ * Description: Append in post_content styled flexible layouts
+ * Version: 1.0
+ * Author: Studio Goliath
+ * Author URI: http://www.studio-goliath.com/
+ */
+
+if (!class_exists('GoliathSimpleBricks')) {
+
+    class GoliathSimpleBricks {
+
+        function __construct() {
+            $this->PLUGIN_DIR_PATH = plugin_dir_path(__FILE__);
+
+            add_filter('the_content', array(&$this, 'display_parts'));
+        }
+
+        /**
+        * Display each brick
+        *
+        * @param $content
+        * @return string
+        */
+        function display_parts($content)
+        {
+            global $row;
+
+            if ( function_exists('have_rows') ) {
+                if ( have_rows('bricks') ) {
+                    while ( have_rows('bricks') ) {
+                        the_row();
+
+                        $layout = get_row_layout();
+
+                        $content .= $this->get_content_for_layout($layout);
+                    }
+                }
+            }
+
+            return $content;
+        }
+
+        function get_content_for_layout($layout)
+        {
+            $template = $this->get_template($layout);
+
+            if (!empty($template)) {
+                require($template);
+            }
+        }
+
+        function get_template($layout)
+        {
+            $template = '';
+
+            $template = locate_template( "/bricks/{$layout}.php", false );
+
+            return $template;
+        }
+    }
+
+    new GoliathSimpleBricks();
+}
