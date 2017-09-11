0a67eb0facd04981313e7ce3dd892317abdf39b2
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-09-11 15:59:33 +0200

    :art: sidebar style

--- dest/wp-content/themes/super/functions.php
+++ dest/wp-content/themes/super/functions.php
@@ -25,10 +25,10 @@ add_theme_support( 'title-tag' );
 show_admin_bar(false);
 
 // Disable Tags
-function super_unregister_tags(){
+function beneteau_unregister_tags(){
     unregister_taxonomy_for_object_type('post_tag', 'post');
 }
-add_action( 'init', 'super_unregister_tags' );
+add_action( 'init', 'beneteau_unregister_tags' );
 
 
 /*-----------------------------------------------------------------------------------*/
@@ -48,13 +48,13 @@ remove_action( 'wp_head', 'rest_output_link_wp_head' );
 remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 
 // remove comment author class
-function super_remove_comment_author_class( $classes ){
+function beneteau_remove_comment_author_class( $classes ){
 	foreach( $classes as $key => $class ){
 		if(strstr($class, 'comment-author-')) unset( $classes[$key] );
 	}
 	return $classes;
 }
-add_filter( 'comment_class' , 'super_remove_comment_author_class' );
+add_filter( 'comment_class' , 'beneteau_remove_comment_author_class' );
 
 // remove login errors
 add_filter( 'login_errors', create_function('$a', "return null;") );
@@ -64,36 +64,36 @@ add_filter( 'login_errors', create_function('$a', "return null;") );
 /* Admin
 /*-----------------------------------------------------------------------------------*/
 // Remove some useless admin stuff
-function super_remove_submenus() {
+function beneteau_remove_submenus() {
   $page = remove_submenu_page( 'themes.php', 'themes.php' );
 }
-add_action( 'admin_menu', 'super_remove_submenus', 999 );
-function super_remove_top_menus( $wp_admin_bar ){
+add_action( 'admin_menu', 'beneteau_remove_submenus', 999 );
+function beneteau_remove_top_menus( $wp_admin_bar ){
     $wp_admin_bar->remove_node( 'wp-logo' );
 }
-add_action( 'admin_bar_menu', 'super_remove_top_menus', 999 );
+add_action( 'admin_bar_menu', 'beneteau_remove_top_menus', 999 );
 
 // Enlever le lien par défaut autour des images
-function super_imagelink_setup(){
+function beneteau_imagelink_setup(){
 	if(get_option( 'image_default_link_type' ) !== 'none') update_option('image_default_link_type', 'none');
 }
-add_action( 'admin_init', 'super_imagelink_setup' );
+add_action( 'admin_init', 'beneteau_imagelink_setup' );
 
 // Enlever les <p> autour des images
-function super_remove_p_around_images($content){
+function beneteau_remove_p_around_images($content){
    return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
-add_filter( 'the_content', 'super_remove_p_around_images' );
+add_filter( 'the_content', 'beneteau_remove_p_around_images' );
 
 // Allow svg in media library
-function super_mime_types($mimes){
+function beneteau_mime_types($mimes){
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 }
-add_filter( 'upload_mimes', 'super_mime_types' );
+add_filter( 'upload_mimes', 'beneteau_mime_types' );
 
 // Custom posts in the dashboard
-function super_right_now_custom_post() {
+function beneteau_right_now_custom_post() {
     $post_types = get_post_types(array( '_builtin' => false ) , 'objects' , 'and');
     foreach($post_types as $post_type){
         $cpt_name = $post_type->name;
@@ -105,23 +105,23 @@ function super_right_now_custom_post() {
         }
     }
 }
-add_action( 'dashboard_glance_items', 'super_right_now_custom_post' );
+add_action( 'dashboard_glance_items', 'beneteau_right_now_custom_post' );
 
 // Customize a bit the wysiwyg editor
-function super_mce_before_init( $styles ){
+function beneteau_mce_before_init( $styles ){
     // Remove h1 and code
     $styles['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6';
     // Let only the colors you want
     $styles['textcolor_map'] = '[' . "'000000', 'Noir', '565656', 'Texte'" . ']';
     return $styles;
 }
-add_filter( 'tiny_mce_before_init', 'super_mce_before_init' );
+add_filter( 'tiny_mce_before_init', 'beneteau_mce_before_init' );
 
 
 /*-----------------------------------------------------------------------------------*/
 /* Markup gallery
 /*-----------------------------------------------------------------------------------*/
-function essor_gallery( $output, $attr){
+function beneteau_gallery( $output, $attr){
     global $post, $wp_locale;
     static $instance = 0;
     $instance++;
@@ -174,7 +174,7 @@ function essor_gallery( $output, $attr){
     $output .= "</ul>";
     return $output;
 }
-add_filter( 'post_gallery', 'essor_gallery' );
+add_filter( 'post_gallery', 'beneteau_gallery' );
 
 
 /*-----------------------------------------------------------------------------------*/
@@ -187,67 +187,67 @@ register_nav_menus(
 );
 
 // Cleanup WP Menu html
-function super_css_attributes_filter($var){
+function beneteau_css_attributes_filter($var){
     return is_array($var) ? array_intersect($var, array('current-menu-item', 'current_page_parent')) : '';
 }
-add_filter( 'nav_menu_css_class', 'super_css_attributes_filter' );
+add_filter( 'nav_menu_css_class', 'beneteau_css_attributes_filter' );
 
 
-/*-----------------------------------------------------------------------------------*/
-/* Sidebar & Widgets
-/*-----------------------------------------------------------------------------------*/
-function super_register_sidebars(){
-	register_sidebar(array(
-		'id' => 'sidebar',
-		'name' => 'Sidebar',
-		'description' => 'Take it on the side...',
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '',
-		'after_title' => '',
-		'empty_title'=> ''
-	));
-}
-add_action( 'widgets_init', 'super_register_sidebars' );
+// /*-----------------------------------------------------------------------------------*/
+// /* Sidebar & Widgets
+// /*-----------------------------------------------------------------------------------*/
+// function super_register_sidebars(){
+// 	register_sidebar(array(
+// 		'id' => 'sidebar',
+// 		'name' => 'Sidebar',
+// 		'description' => 'Take it on the side...',
+// 		'before_widget' => '',
+// 		'after_widget' => '',
+// 		'before_title' => '',
+// 		'after_title' => '',
+// 		'empty_title'=> ''
+// 	));
+// }
+// add_action( 'widgets_init', 'super_register_sidebars' );
 
-// Deregister default widgets
-function super_unregister_default_widgets(){
-    unregister_widget('WP_Widget_Pages');
-    unregister_widget('WP_Widget_Calendar');
-    unregister_widget('WP_Widget_Archives');
-    unregister_widget('WP_Widget_Links');
-    unregister_widget('WP_Widget_Meta');
-    unregister_widget('WP_Widget_Search');
-    unregister_widget('WP_Widget_Text');
-    unregister_widget('WP_Widget_Categories');
-    unregister_widget('WP_Widget_Recent_Posts');
-    unregister_widget('WP_Widget_Recent_Comments');
-    unregister_widget('WP_Widget_RSS');
-    unregister_widget('WP_Widget_Tag_Cloud');
-    unregister_widget('WP_Nav_Menu_Widget');
-}
-add_action( 'widgets_init', 'super_unregister_default_widgets' );
+// // Deregister default widgets
+// function super_unregister_default_widgets(){
+//     unregister_widget('WP_Widget_Pages');
+//     unregister_widget('WP_Widget_Calendar');
+//     unregister_widget('WP_Widget_Archives');
+//     unregister_widget('WP_Widget_Links');
+//     unregister_widget('WP_Widget_Meta');
+//     unregister_widget('WP_Widget_Search');
+//     unregister_widget('WP_Widget_Text');
+//     unregister_widget('WP_Widget_Categories');
+//     unregister_widget('WP_Widget_Recent_Posts');
+//     unregister_widget('WP_Widget_Recent_Comments');
+//     unregister_widget('WP_Widget_RSS');
+//     unregister_widget('WP_Widget_Tag_Cloud');
+//     unregister_widget('WP_Nav_Menu_Widget');
+// }
+// add_action( 'widgets_init', 'super_unregister_default_widgets' );
 
 
 /*-----------------------------------------------------------------------------------*/
 /* Enqueue Styles and Scripts
 /*-----------------------------------------------------------------------------------*/
-function super_scripts(){
+function beneteau_scripts(){
     // header
-	wp_enqueue_style( 'super-style', get_template_directory_uri() . '/css/main.css', array(), SUPER_VERSION );
+	wp_enqueue_style( 'beneteau-style', get_template_directory_uri() . '/css/main.css', array(), BENETEAU_VERSION );
 
 	// footer
 	wp_deregister_script('jquery');
-	wp_enqueue_script( 'super-scripts', get_template_directory_uri() . '/js/main.js', array(), SUPER_VERSION, true );
+	wp_enqueue_script( 'beneteau-scripts', get_template_directory_uri() . '/js/main.js', array(), BENETEAU_VERSION, true );
 
     wp_deregister_script( 'wp-embed' );
 }
-add_action( 'wp_enqueue_scripts', 'super_scripts' );
+add_action( 'wp_enqueue_scripts', 'beneteau_scripts' );
 
 /*-----------------------------------------------------------------------------------*/
 /* Post types
 /*-----------------------------------------------------------------------------------*/
-function super_post_type(){
+function beneteau_post_type(){
     register_post_type( 'brand', array(
         'label' => 'Marques',
         'singular_label' => 'Marque',
@@ -256,9 +256,9 @@ function super_post_type(){
         'supports' => array('title', 'editor', 'revisions'),
     ) );
 }
-add_action( 'init', 'super_post_type' );
+add_action( 'init', 'beneteau_post_type' );
 
-function super_taxonomies(){
+function beneteau_taxonomies(){
     register_taxonomy( 'branch', array('brand'), array(
         'label' => 'Secteur',
         'singular_label' => 'Secteur',
@@ -266,4 +266,4 @@ function super_taxonomies(){
         'show_admin_column' => true
     ) );
 }
-add_action( 'init', 'super_taxonomies' );
+add_action( 'init', 'beneteau_taxonomies' );
