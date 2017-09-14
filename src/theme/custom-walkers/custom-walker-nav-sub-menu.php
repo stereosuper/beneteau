d50f25bb2f29fccfbc43cc6b8f7e9abeca46a042
Alain Diart <adiart@studio-goliath.com>
2017-09-14 12:12:26 +0200

    Modification du walker de menu en sous-page pour les ancres dans la page

--- src/theme/custom-walkers/custom-walker-nav-sub-menu.php
+++ src/theme/custom-walkers/custom-walker-nav-sub-menu.php
@@ -5,92 +5,67 @@
 
 class CustomWalkerNavSubMenu extends Walker_Nav_Menu
 {
-    private $ID;
-    private $depth;
-    private $classes = array();
-    private $child_count = 0;
-    private $have_current = false;
+    private $last_depth = 0;
+    private $is_in_current_path = false;
 
+    private function ancestorOfCurrent($item, $depth)
+    {
+        if ($this->last_depth >= $depth) {
+            $this->is_in_current_path = false;
+        }
+
+        if (!$this->is_in_current_path) {
+            $current_element_markers = array( 'current-menu-item', 'current-menu-parent', 'current-menu-ancestor' );
+            $found_classes = array_intersect( $current_element_markers, $item->classes );
+            $ancestor_of_current = !empty($found_classes);
+
+            if ($ancestor_of_current) {
+                $this->last_depth = $depth;
+                $this->is_in_current_path = $ancestor_of_current;
+            }
+        }
+    }
 
     // Don't start the top level
-    function start_lvl(&$output, $depth=0, $args=array()) {
-
-        if( 0 == $depth || $this->depth != $depth )
+    function start_lvl(&$output, $depth=0, $args=array())
+    {
+        if( 0 == $depth )
             return;
 
-        parent::start_lvl($output, $depth,$args);
+        parent::start_lvl($output, $depth, $args);
     }
 
     // Don't end the top level
-    function end_lvl(&$output, $depth=0, $args=array()) {
-        if( 0 == $depth || $this->depth != $depth )
+    function end_lvl(&$output, $depth=0, $args=array())
+    {
+        if( 0 == $depth )
             return;
 
-        parent::end_lvl($output, $depth,$args);
+        parent::end_lvl($output, $depth, $args);
     }
 
     // Don't print top-level elements
-    function start_el(&$output, $item, $depth=0, $args=array()) {
+    function start_el(&$output, $item, $depth=0, $args=array())
+    {
+        $this->ancestorOfCurrent($item, $depth);
 
-        $is_current = in_array('current-menu-item', $this->classes);
-
-        if( ! $is_current )
+        if (  0 == $depth || !$this->is_in_current_path )
             return;
 
         parent::start_el($output, $item, $depth, $args);
     }
 
-    function end_el(&$output, $item, $depth=0, $args=array()) {
-        if( 0 == $depth )
+    function end_el(&$output, $item, $depth=0, $args=array())
+    {
+        if (  0 == $depth || !$this->is_in_current_path )
             return;
 
         parent::end_el($output, $item, $depth, $args);
     }
 
     // Only follow down one branch
-    function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output ) {
-
-        // Check if element is in the current tree to display
-        $current_element_markers = array( 'current-menu-item', 'current-menu-parent', 'current-menu-ancestor' );
-        $this->classes = array_intersect( $current_element_markers, $element->classes );
-
-        // If element has a 'current' class, it is an ancestor of the current element
-        $ancestor_of_current = !empty($this->classes);
-
-        // check if the element is the actual page element we are on.
-        $is_current = in_array('current-menu-item', $this->classes);
-
-        // if it is the current element
-        if($is_current) {
-
-            // set the count / ID / and depth to use in the other functions.
-            $this->child_count = ( isset($children_elements[$element->ID]) ) ? count($children_elements[$element->ID]) : 0;
-            $this->ID = $element->ID;
-            $this->depth = $depth;
-            $this->have_current = true;
-
-            if($this->child_count > 0) {
-
-                // if there are children loop through them and display the kids.
-                foreach( $children_elements[$element->ID] as $child ) {
-                    parent::display_element( $child, $children_elements, $max_depth, $depth, $args, $output );
-                }
-
-            } else {
-                // no children so loop through kids of parent item.
-                foreach( $children_elements[$element->menu_item_parent] as $child ) {
-                    parent::display_element( $child, $children_elements, $max_depth, $depth, $args, $output );
-                }
-
-            }
-        }
-
-        // if depth is zero and not in current tree go to the next element
-        if ( 0 == $depth && !$ancestor_of_current)
-            return;
-
-        // if we aren't on the current element proceed as normal
-        if(! $this->have_current )
-            parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+    function display_element( $element, &$children_elements, $max_depth, $depth=0, $args, &$output )
+    {
+        parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
     }
-    }
+}
