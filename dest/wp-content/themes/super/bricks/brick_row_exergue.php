f36ae8d4d818792aa422cfb7a18b0ff8380c92e2
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-09-19 12:15:53 +0200

    :art: highlighted with img

--- dest/wp-content/themes/super/bricks/brick_row_exergue.php
+++ dest/wp-content/themes/super/bricks/brick_row_exergue.php
@@ -5,12 +5,16 @@ $anchor = get_sub_field('anchor');
 $blocks = get_sub_field('blocks');
 $title = get_sub_field('title');
 $content = get_sub_field('content');
+$image = get_sub_field('image');
 
 ?>
 
+<?php if (!empty($image)) : ?>
+    <style>.highlighted:before{ background-image: url(<?php echo $image; ?>); }</style>
+<?php endif; ?>
 
 <?php if (!empty($anchor)) : ?><a name="<?php echo $anchor; ?>"></a><?php endif; ?>
-<div class='highlighted'>
+<div class='highlighted <?php if (empty($image)) : ?> no-img <?php endif; ?>'>
     <?php if (!empty($title)) : ?><span class='title'><?php echo $title; ?></span><?php endif; ?>
     <?php if (!empty($content)) : ?><strong><?php echo $content; ?></strong><?php endif; ?>
 </div>
