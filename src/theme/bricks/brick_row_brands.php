0d4eca5265581c93e6d2c95727a873bce33f4486
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-09-21 14:57:40 +0200

    :art: ACF contact

--- src/theme/bricks/brick_row_brands.php
+++ src/theme/bricks/brick_row_brands.php
@@ -1,5 +1,4 @@
 <?php
-global $post;
 
 // Champs possibles pour le row
 $anchor = get_sub_field('anchor');
@@ -9,51 +8,32 @@ $title_align = get_sub_field('title_align');
 ?>
 
 <?php if (!empty($anchor)) : ?><a name="<?php echo $anchor; ?>"></a><?php endif; ?>
-<div class='push-wrapper'>
+
     <?php if (!empty($title)) : ?>
         <h2 class="align<?php echo $title_align; ?>"><?php echo $title; ?></h2>
     <?php endif; ?>
 
-    <div class='push-container'>
+    <?php if (have_rows('blocks')) : ?>
+        <div class='grid grid-brands'>
+            <?php while (have_rows('blocks')) :
+                the_row();
 
-        <?php
-            // Cf. http://codex.wordpress.org/Class_Reference/WP_Query
-            $query_args = array(
-                // Type Parameters
-                'post_type' => 'brand',                // (string / array) - use post types. Retrieves posts by Post Types, default value is 'post'; 'post' - a post. 'page' - a page. 'revision' - a revision. 'attachment' - an attachment. The default WP_Query sets 'post_status'=>'publish', but attachments default to 'post_status'=>'inherit' so you'll need to set the status to 'inherit' or 'any'. 'any' - retrieves any type except revisions and types with 'exclude_from_search' set to true. Custom Post Types (e.g. movies)
-                // Status Parameters
-                'post_status' => 'publish',              // (string / array) - use post status. Retrieves posts by Post Status. Default value is 'publish', but if the user is logged in, 'private' is added. And if the query is run in an admin context (administration area or AJAX call), protected statuses are added too. By default protected statuses are 'future', 'draft' and 'pending'. 'publish' - a published post or page. 'pending' - post is pending review. 'draft' - a post in draft status. 'auto-draft' - a newly created post, with no content. 'future' - a post to publish in the future. 'private' - not visible to users who are not logged in. 'inherit' - a revision. see get_children. 'trash' - post is in trashbin (available with Version 2.9). 'any' - retrieves any status except those from post types with 'exclude_from_search' set to true.
-                // Pagination Parameters
-                'posts_per_page' => -1,           // (int) - number of post to show per page (available with Version 2.1, replaced showposts parameter). Use 'posts_per_page'=>-1 to show all posts. Set the 'paged' parameter if pagination is off after using this parameter. Note: if the query is in a feed, wordpress overwrites this parameter with the stored 'posts_per_rss' option. To reimpose the limit, try using the 'post_limits' filter, or filter 'pre_option_posts_per_rss' and return -1
-            );
-
-            $brand_query = new WP_Query($query_args);
-            if ( $brand_query->have_posts() ) :
-                while ( $brand_query->have_posts() ) :
-                    $brand_query->the_post();
-
-                    $logo = super_get_field('logo');
-                    $website = super_get_field('website');
-                    $contact_type = super_get_field('contact_type');
-                    if ($contact_type=='url') {
-                        $link_url = super_get_field('contact_url');
-                    } else {
-                        $link_url = 'mailto:'.super_get_field('contact_email');
-                    }
-                    $excerpt = super_get_field('contact_excerpt');
+                $title = get_sub_field('title');
+                $image = get_sub_field('image');
+                $image_url = '';
+                if(is_array($image) && isset($image['ID'])) {
+                    list($image_url, $w, $h) = wp_get_attachment_image_src($image['ID'], 'medium');
+                }
+                $link = get_sub_field('link');
+                $content = get_sub_field('content');
             ?>
-                    <a href='<?php echo $link_url; ?>'>
-                        <div class="logo">
-                            <?php if (!empty($logo)) : ?><img src="<?php echo $logo; ?>" alt="<?php echo esc_attr(get_the_title()); ?>" /><?php endif; ?>
-                            <?php if (empty($logo)) : ?><?php the_title(); ?><?php endif; ?>
-                        </div>
-                        <?php if (!empty($excerpt)) : ?><p><?php echo $excerpt; ?></p><?php endif; ?>
-                        <span class='link'><?php _e('Contacter', 'beneteau'); ?></span>
-                    </a>
-            <?php
-                endwhile; // while ( $brand_query->have_posts() ) :
-            endif; // if ( $brand_query->have_posts() ) :
-        ?>
 
-    </div>
-</div>
+                <div class='col-3'>
+                    <div class='img'><?php if (!empty($image_url)) : ?><img src='<?php echo $image_url; ?>' alt='<?php echo $title; ?>'><?php endif; ?></div>
+                    <h2><?php echo $title; ?></h2>
+                    <?php if (!empty($content)) : ?><?php echo $content; ?><?php endif; ?>
+                    <a href='<?php echo $link; ?>' class='btn-invert-block'><?php _e('Contacter', 'beneteau'); ?></a>
+                </div>
+            <?php endwhile ?>
+        </div>
+    <?php endif; ?>
