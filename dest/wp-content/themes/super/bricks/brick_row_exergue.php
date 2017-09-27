7e9611b72641d40732f1387f64c57411aa053189
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-09-27 14:58:35 +0200

    :art: highlighted gradient

--- dest/wp-content/themes/super/bricks/brick_row_exergue.php
+++ dest/wp-content/themes/super/bricks/brick_row_exergue.php
@@ -9,11 +9,8 @@ $image = get_sub_field('image');
 
 ?>
 
-<?php if (!empty($image)) : ?>
-    <style>.highlighted:before{ background-image: url(<?php echo $image; ?>); }</style>
-<?php endif; ?>
-
 <div class='isAnimated highlighted <?php if (empty($image)) : ?> no-img <?php endif; ?>' id='<?php echo $anchor; ?>'>
+    <?php if (!empty($image)) : ?><span class='bg-img' style='background-image: url(<?php echo $image; ?>);'></span><?php endif; ?>
     <?php if (!empty($title)) : ?><span class='title'><?php echo $title; ?></span><?php endif; ?>
     <?php if (!empty($content)) : ?><strong><?php echo $content; ?></strong><?php endif; ?>
 </div>
