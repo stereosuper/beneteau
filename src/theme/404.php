6fdfcac8efb5ee48ea3f14eef3951c0290e097cc
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-09-27 17:16:08 +0200

    :art: 404

--- src/theme/404.php
+++ src/theme/404.php
@@ -1,5 +1,9 @@
 <?php get_header(); ?>
 
+<div class='container'>
 	<h1>404</h1>
+	<p><?php _e("We're sorry, the page you're looking for doesn't exist.", 'beneteau'); ?></p>
+	<p><a href='<?php echo site_url(); ?>'><?php _e("Go back to home", 'beneteau'); ?></a></p>
+</div>
 
 <?php get_footer(); ?>
