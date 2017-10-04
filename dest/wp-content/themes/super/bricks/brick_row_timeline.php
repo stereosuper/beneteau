3d049348a4efb6a5d614090c2cbb323a02716a40
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-10-04 14:22:32 +0200

    :art: timeline module

---
+++ dest/wp-content/themes/super/bricks/brick_row_timeline.php
@@ -0,0 +1,40 @@
+<?php
+
+// Champs possibles pour le row
+$anchor = get_sub_field('anchor');
+$title = get_sub_field('title');
+$title_align = get_sub_field('title_align');
+
+?>
+
+<div class='isAnimated'>
+    <?php if (!empty($title)){ ?>
+        <h2 id='<?php echo $anchor; ?>' class="align<?php echo $title_align; ?>"><?php echo $title; ?></h2>
+    <?php }else if(!empty($anchor)){ ?>
+        <div id='<?php echo $anchor; ?>'></div>
+    <?php } ?>
+
+    <?php if (super_have_rows('dates')) : ?>
+        <ul class='history'>
+            <?php while (super_have_rows('dates')) :
+                the_row();
+
+                $date = get_sub_field('date');
+                $text = get_sub_field('text');
+                $link = get_sub_field('link');
+            ?>
+
+                <li class='isAnimated'>
+                    <time datetime='<?php echo $date; ?>'><?php echo $date; ?></time>
+                    <?php echo wp_get_attachment_image( get_sub_field('img'), 'full' ); ?>
+                    <?php if( !empty($text) ) : ?>
+                        <p><?php echo $text; ?></p>
+                    <?php endif; ?>
+                    <?php if( !empty($link) ) : ?>
+                        <a href='<?php echo $link; ?>' class='link'><?php _e('En savoir plus', 'benteau'); ?></a>
+                    <?php endif; ?>
+                </li>
+            <?php endwhile ?>
+        </ul>
+    <?php endif; ?>
+</div>
