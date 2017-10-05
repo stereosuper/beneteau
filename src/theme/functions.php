9922bab4506b3437559e04fcc35da65ede7543ac
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-10-05 12:30:59 +0200

    :art: activate widgets

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -258,40 +258,40 @@ add_filter('next_post_link', 'beneteau_next_class');
 add_filter('previous_post_link', 'beneteau_prev_class');
 
 
-// /*-----------------------------------------------------------------------------------*/
-// /* Sidebar & Widgets
-// /*-----------------------------------------------------------------------------------*/
-// function super_register_sidebars(){
-// 	register_sidebar(array(
-// 		'id' => 'sidebar',
-// 		'name' => 'Sidebar',
-// 		'description' => 'Take it on the side...',
-// 		'before_widget' => '',
-// 		'after_widget' => '',
-// 		'before_title' => '',
-// 		'after_title' => '',
-// 		'empty_title'=> ''
-// 	));
-// }
-// add_action( 'widgets_init', 'super_register_sidebars' );
+/*-----------------------------------------------------------------------------------*/
+/* Sidebar & Widgets
+/*-----------------------------------------------------------------------------------*/
+function super_register_sidebars(){
+	register_sidebar(array(
+		'id' => 'sidebar',
+		'name' => 'Sidebar',
+		'description' => 'Take it on the side...',
+		'before_widget' => '',
+		'after_widget' => '',
+		'before_title' => '',
+		'after_title' => '',
+		'empty_title'=> ''
+	));
+}
+add_action( 'widgets_init', 'super_register_sidebars' );
 
-// // Deregister default widgets
-// function super_unregister_default_widgets(){
-//     unregister_widget('WP_Widget_Pages');
-//     unregister_widget('WP_Widget_Calendar');
-//     unregister_widget('WP_Widget_Archives');
-//     unregister_widget('WP_Widget_Links');
-//     unregister_widget('WP_Widget_Meta');
-//     unregister_widget('WP_Widget_Search');
-//     unregister_widget('WP_Widget_Text');
-//     unregister_widget('WP_Widget_Categories');
-//     unregister_widget('WP_Widget_Recent_Posts');
-//     unregister_widget('WP_Widget_Recent_Comments');
-//     unregister_widget('WP_Widget_RSS');
-//     unregister_widget('WP_Widget_Tag_Cloud');
-//     unregister_widget('WP_Nav_Menu_Widget');
-// }
-// add_action( 'widgets_init', 'super_unregister_default_widgets' );
+// Deregister default widgets
+function super_unregister_default_widgets(){
+    unregister_widget('WP_Widget_Pages');
+    unregister_widget('WP_Widget_Calendar');
+    unregister_widget('WP_Widget_Archives');
+    unregister_widget('WP_Widget_Links');
+    unregister_widget('WP_Widget_Meta');
+    unregister_widget('WP_Widget_Search');
+    unregister_widget('WP_Widget_Text');
+    unregister_widget('WP_Widget_Categories');
+    unregister_widget('WP_Widget_Recent_Posts');
+    unregister_widget('WP_Widget_Recent_Comments');
+    unregister_widget('WP_Widget_RSS');
+    unregister_widget('WP_Widget_Tag_Cloud');
+    unregister_widget('WP_Nav_Menu_Widget');
+}
+add_action( 'widgets_init', 'super_unregister_default_widgets' );
 
 
 /*-----------------------------------------------------------------------------------*/
