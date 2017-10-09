a34b4476846ff7a7e8a34cc00ab9817119e24df9
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-10-09 16:39:43 +0200

    :art: eolia tpl apply

---
+++ dest/wp-content/themes/super/tpl-apply.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Template Name: Candidature
+*/
+
+get_header(); ?>
+
+<?php if ( have_posts() ) : the_post(); ?>
+
+	<?php $sidebar_menu = wp_nav_menu( array(
+        'echo' => false,
+        'theme_location' => 'job',
+        'container' => false,
+        'menu_class' => 'sidebar-menu',
+        'menu_id' => 'submenu',
+        ) );
+    ?>
+
+	<div class='container<?php echo (strpos($sidebar_menu, '<li')!==FALSE)?' container-sidebar':''; ?>'>
+
+		<?php if (strpos($sidebar_menu, '<li')!==FALSE) : ?>
+			<aside class='sidebar wrapper-sticky' id='sidebar'>
+				<div class='content-sidebar' id='blockSticky'>
+					<span class='logo-reduced'></span>
+					<span class='bg-sidebar'></span>
+					<?php echo $sidebar_menu; ?>
+				</div>
+			</aside>
+		<?php endif; ?>
+
+		<div class='content'>
+			<?php if( function_exists('yoast_breadcrumb') ){ yoast_breadcrumb('<div class="breadcrumbs">','</div>'); } ?>
+
+			<h1 class='isAnimated'><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+		</div>
+
+	</div>
+
+<?php else : ?>
+	<div class='container'>
+		<h1>404</h1>
+	</div>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
