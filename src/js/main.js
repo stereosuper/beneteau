8f8780654a68bdff60f5d0943d84a1bad02bbf43
adrienlementheour <adrien.le.mentheour@gmail.com>
2017-10-10 11:22:17 +0200

    :bug: ie11 push bg

--- src/js/main.js
+++ src/js/main.js
@@ -83,6 +83,8 @@ $(function(){
     // Since script is loaded asynchronously, load event isn't always fired !!!
     document.readyState === 'complete' ? loadHandler() : $(window).on('load', loadHandler);
 
+    if(!(window.ActiveXObject) && "ActiveXObject" in window) body.addClass('ie11');
+
     $(window).on('resize', throttle(function(){
         requestAnimFrame(resizeHandler);
     }, 60));
