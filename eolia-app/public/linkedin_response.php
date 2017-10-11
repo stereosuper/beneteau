37ec56ae510878e1dbf29a5103d1f66975484bbe
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-10-11 14:43:25 +0200

    oups

--- eolia-app/public/linkedin_response.php
+++
@@ -1,9 +0,0 @@
-<?php
-@session_start();
-if ( isset( $_GET['code'] ) && isset( $_GET['state'] ) ) {
-	if ( $_SESSION['linkedin_state'] == $_GET['state'] ) {
-		$_SESSION['linkedin_code'] = $_GET['code'];
-	}
-}
-echo '<script>window.close();</script>';
-?>
--- eolia-app/public/viadeo_response.php
+++
@@ -1,9 +0,0 @@
-<?php
-@session_start();
-if ( isset( $_GET['code'] ) ) {
-	if ( $_SESSION['viadeo_state'] == $_GET['state'] ) {
-		$_SESSION['viadeo_code'] = $_GET['code'];
-	}
-}
-echo '<script>window.close();</script>';
-?>
