90dc4ec251d97f7a6a2a535cd73c3bdff32e25df
Elisabeth Hamel <elisabeth@stereosuper.fr>
2017-11-28 14:07:19 +0100

    :art: remove widget job button

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -281,7 +281,7 @@ function super_register_sidebars(){
 		'name' => 'Emploi',
 		'description' => "Apparait sur la page d'accueil Emploi",
 		'before_widget' => '',
-		'after_widget' => '<a href="' . super_get_field('jobs_link', 'options') . '" class="btn">' . __('All our offers', 'beneteau') . '</a>',
+		'after_widget' => '',
 		'before_title' => '<h2>',
 		'after_title' => '</h2>',
 		'empty_title'=> ''
