365086452ee17b89e2f8ec7b710828bd7a4439af
Alphability <albanmezino@gmail.com>
2019-05-28 16:47:15 +0200

    :art: home offers desktop
    Signed-off-by: Alphability <albanmezino@gmail.com>

--- dest/wp-content/themes/super/bricks/brick_row_home_offers.php
+++ dest/wp-content/themes/super/bricks/brick_row_home_offers.php
@@ -1,32 +1,36 @@
-<div class="home-offers container">
-    <?php
-    if ($image_offers = get_sub_field('image_offers')): ?>
-        <h2 class="home-offers-image">
-            <?php echo wp_get_attachment_image($image_offers['ID'], 'full'); ?>
-        </h2>
-    <?php endif; ?>
-    <?php if ($content_offers = get_sub_field('content_offers')): ?>
-        <p class="home-offers-description"><?php echo $content_offers ?></p>
-    <?php endif; ?>
-    <?php 
-    $title_offers_first_part = get_sub_field('title_offers_first_part');
-    $title_offers_second_part = get_sub_field('title_offers_second_part');
-    $title_offers_third_part = get_sub_field('title_offers_third_part');
-    if ($title_offers_first_part && $title_offers_second_part && $title_offers_third_part):
-    ?>
-        <h3 class="home-offers-title">
-            <?php echo $title_offers_first_part ?>
-            <span><?php echo $title_offers_second_part ?></span>
-            <?php echo $title_offers_third_part ?>
-        </h3>
-    <?php endif; ?>
-    <?php if ($link_offers = get_sub_field('link_offers')): ?>
-        <p>
-            <a class="a-btn a-btn-light home-offers-link" href="<?php echo $link_offers['url']; ?>" title="<?php echo htmlspecialchars(strip_tags($link_offers['title']), ENT_QUOTES); ?>" target="<?php echo $link_offers['target']; ?>" <?php echo $link_offers['target'] === '_blank' ? 'rel="noopener noreferrer"' : ''; ?>>
-            <?php echo $link_offers['title'] ?>
-            </a>
-        </p>
-    <?php endif; ?>
+<div class="home-offers">
+    <div class="container">
+        <?php
+        if ($image_offers = get_sub_field('image_offers')): ?>
+            <h2 class="home-offers-image">
+                <?php echo wp_get_attachment_image($image_offers['ID'], 'full'); ?>
+            </h2>
+        <?php endif; ?>
+        <?php if ($content_offers = get_sub_field('content_offers')): ?>
+            <p class="home-offers-description"><?php echo $content_offers ?></p>
+        <?php endif; ?>
+        <div class="last-part-wrapper">
+            <?php 
+            $title_offers_first_part = get_sub_field('title_offers_first_part');
+            $title_offers_second_part = get_sub_field('title_offers_second_part');
+            $title_offers_third_part = get_sub_field('title_offers_third_part');
+            if ($title_offers_first_part && $title_offers_second_part && $title_offers_third_part):
+            ?>
+                <h3 class="home-offers-title">
+                    <?php echo $title_offers_first_part ?>
+                    <span><?php echo $title_offers_second_part ?></span>
+                    <?php echo $title_offers_third_part ?>
+                </h3>
+            <?php endif; ?>
+            <?php if ($link_offers = get_sub_field('link_offers')): ?>
+                <p>
+                    <a class="a-btn a-btn-light home-offers-link" href="<?php echo $link_offers['url']; ?>" title="<?php echo htmlspecialchars(strip_tags($link_offers['title']), ENT_QUOTES); ?>" target="<?php echo $link_offers['target']; ?>" <?php echo $link_offers['target'] === '_blank' ? 'rel="noopener noreferrer"' : ''; ?>>
+                    <?php echo $link_offers['title'] ?>
+                    </a>
+                </p>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
 <div class="home-professions">
     <?php if ($video_professions_id = get_sub_field('video_professions_id')): ?>
