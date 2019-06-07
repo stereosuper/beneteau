91f98026a87d12abde912470d59170b7f117dd7b
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-06-07 17:45:59 +0200

    :art: menu close

--- dest/wp-content/themes/super/js/main.js
+++ dest/wp-content/themes/super/js/main.js
@@ -35660,23 +35660,31 @@ module.exports = function (htmlAze, header) {
     if (!header.length) return;
 
     var nav = $('#nav');
-    var menuBg = $('#menuBg');
+    //const menuBg = $('#menuBg');
+    var close = $('#main-navigation-cross');
 
     header.on('click', '#burger', function (e) {
         e.preventDefault();
         nav.addClass('on');
         htmlAze.addClass('menu-open');
-        menuBg.addClass('on');
+        //menuBg.addClass('on');
     }).on('click', '.js-menu-btn', function () {
         $(this).toggleClass('on').parent().find('.menu-content').toggleClass('on').parent().siblings().find('.menu-content').removeClass('on').parent().find('.js-menu-btn').removeClass('on');
-        $(this).hasClass('on') ? header.addClass('on') : header.removeClass('on');
+        if ($(this).hasClass('on')) {
+            header.addClass('on');
+            close.css('top', $(this).parent().find('.menu-content').offset().top + $(this).parent().find('.menu-content').outerHeight());
+        } else {
+            header.removeClass('on');
+        }
+    }).on('click', '#main-navigation-cross', function () {
+        header.removeClass('on').find('.js-menu-btn').removeClass('on').parent().find('.menu-content').removeClass('on');
     });
 
     header.on('click', '#main-navigation-cross', function (e) {
         e.preventDefault();
         nav.removeClass('on');
         htmlAze.removeClass('menu-open');
-        menuBg.removeClass('on');
+        //menuBg.removeClass('on');
     });
 };
 
--- src/js/header.js
+++ src/js/header.js
@@ -6,24 +6,33 @@ module.exports = function(htmlAze, header) {
     if (!header.length) return;
 
     const nav = $('#nav');
-    const menuBg = $('#menuBg');
+    //const menuBg = $('#menuBg');
+    const close = $('#main-navigation-cross');
 
     header
         .on('click', '#burger', (e) => {
             e.preventDefault();
             nav.addClass('on');
             htmlAze.addClass('menu-open');
-            menuBg.addClass('on');
+            //menuBg.addClass('on');
         })
         .on('click', '.js-menu-btn', function() {
             $(this).toggleClass('on').parent().find('.menu-content').toggleClass('on').parent().siblings().find('.menu-content').removeClass('on').parent().find('.js-menu-btn').removeClass('on');
-            $(this).hasClass('on') ? header.addClass('on') : header.removeClass('on');
+            if( $(this).hasClass('on') ){
+                header.addClass('on');
+                close.css('top', $(this).parent().find('.menu-content').offset().top + $(this).parent().find('.menu-content').outerHeight());
+            }else{
+                header.removeClass('on');
+            }
+        })
+        .on('click', '#main-navigation-cross', function(){
+            header.removeClass('on').find('.js-menu-btn').removeClass('on').parent().find('.menu-content').removeClass('on');
         });
 
     header.on('click', '#main-navigation-cross', e => {
         e.preventDefault();
         nav.removeClass('on');
         htmlAze.removeClass('menu-open');
-        menuBg.removeClass('on');
+        //menuBg.removeClass('on');
     });
 };
