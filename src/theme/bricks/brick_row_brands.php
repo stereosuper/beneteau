32d6ff861f2ad5a419ee13226e26eb2b52c48d3c
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-06-24 15:11:41 +0200

    :bug: footer bg fix #100

--- src/theme/bricks/brick_row_brands.php
+++ src/theme/bricks/brick_row_brands.php
@@ -8,7 +8,7 @@ $title_align = get_sub_field('title_align');
 ?>
     <div>
         <?php if (!empty($title)){ ?>
-            <h2 id='<?php echo $anchor; ?>' class="align<?php echo $title_align; ?>"><?php echo $title; ?></h2>
+            <h2 id='<?php echo $anchor; ?>' class="align<?php echo $title_align; ?>" tabindex='0'><?php echo $title; ?></h2>
         <?php }else if(!empty($anchor)){ ?>
             <div id='<?php echo $anchor; ?>'></div>
         <?php } ?>
