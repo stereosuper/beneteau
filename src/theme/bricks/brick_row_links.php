b4325acae54069c459d25fa6d2c83d74b5580631
Elisabeth Hamel <elisabeth@stereosuper.fr>
2019-06-26 15:57:48 +0200

    :bug: fix #103 push responsive

--- src/theme/bricks/brick_row_links.php
+++ src/theme/bricks/brick_row_links.php
@@ -8,7 +8,6 @@ $title_align = get_sub_field('title_align');
 ?>
 
 <div class='push-wrapper'>
-    <div class='push-banner'></div>
     <?php if (!empty($title)){ ?>
         <h2 id='<?php echo $anchor; ?>' class="align<?php echo $title_align; ?>" tabindex='0'><?php echo $title; ?></h2>
     <?php }else if(!empty($anchor)){ ?>
