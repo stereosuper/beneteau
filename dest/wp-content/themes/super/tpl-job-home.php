80d52ab8e900ada3f8b7be5a6de108ab5b2505c5
Clément Lemoine <clementlemoine.cl@gmail.com>
2019-08-29 14:58:24 +0200

    ✨ add successfactor on job page

--- dest/wp-content/themes/super/tpl-job-home.php
+++ dest/wp-content/themes/super/tpl-job-home.php
@@ -34,7 +34,27 @@ get_header(); ?>
 
 			<h1 class='isAnimated'><?php the_title(); ?></h1>
             <?php the_content(); ?>
-            <?php dynamic_sidebar( 'job' ); ?>
+            <form id="keywordsearch" class='sf-form' name="keywordsearch" method="get" action="https://beneteau-group.jobs2web.com/search/" lang="en_US" xml:lang="en_US">
+                <div class='sf-field'>
+                    <label for='keywordsearch-q'><?php _e('Search by Keyword', 'beneteau'); ?></label>
+                    <input id="keywordsearch-q" name="q" type="text" value="" title="<?php _e('Search by Keyword', 'beneteau'); ?>"/>
+                </div>
+                <div class='sf-field'>
+                    <label for='keywordsearch-locationsearch'><?php _e('Search by Location', 'beneteau'); ?></label>                
+                    <input id="keywordsearch-locationsearch" name="locationsearch" type="text" value="" title="<?php _e('Search by Location', 'beneteau'); ?>"/> <input class="keywordsearch-source" type="hidden" name="utm_source" value="CSSearchWidget" /> 
+                </div>
+                <input class="keywordsearch-startrow" type="hidden" name="startrow" value="1" /> 
+                <button id="keywordsearch-button" type="submit"><?php _e('Search', 'beneteau'); ?></button> 
+            </form>
+            <script src="https://code.jquery.com/jquery-1.7.2.min.js"></script> 
+            <script src="https://beneteau-group.jobs2web.com/view/widgets/keywordsearchHandler.js"></script>
+
+
+            <iframe src='https://rmk-map-2.valhalla2.stage.jobs2web.com/map/?esid=HI2MHAJYg9DEYIQhTPsdSw%3D%3D&locale=en_US&uselcl=false&watercolor=%23FFFFFF&jobdomain=beneteau.valhalla2.stage.jobs2web.com &maplbljob=Job&maplbljobs=jobs&mapbtnsearchjobs=Search+jobs&centerpoint=35.1,31.3&mapzoom=1&keyword=&regionCode=US&parentURL=http%3A%2F%2F beneteau.valhalla2.stage.jobs2web.com%2Fcontent%2FSearch-by-Location%2F%3Flocale%3Dfr_FR' class='sf-map' frameborder='0' style='width:100%;height:400px;'></iframe>
+
+            <?php
+                
+                dynamic_sidebar( 'job' ); ?>
 		</div>
 
 	</div>
