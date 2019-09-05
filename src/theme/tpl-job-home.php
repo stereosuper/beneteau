5d7e4d7e2f1e88b28ece56379cf90de3a94e1dc3
Clément Lemoine <clementlemoine.cl@gmail.com>
2019-09-05 17:40:36 +0200

    ✨ prod update

--- src/theme/tpl-job-home.php
+++ src/theme/tpl-job-home.php
@@ -34,7 +34,7 @@ get_header(); ?>
 
 			<h1 class='isAnimated'><?php the_title(); ?></h1>
             <?php the_content(); ?>
-            <form id="keywordsearch" class='sf-form' name="keywordsearch" method="get" action="https://beneteau-group.jobs2web.com/search/" lang="en_US" xml:lang="en_US">
+            <form id="keywordsearch" class='sf-form' name="keywordsearch" method="get" action="https://jobs.beneteau-group.com/search/" lang="<?php _e('en_US', 'beneteau'); ?>" xml:lang="<?php _e('en_US', 'beneteau'); ?>">
                 <div class='sf-field'>
                     <label for='keywordsearch-q'><?php _e('Search by Keyword', 'beneteau'); ?></label>
                     <input id="keywordsearch-q" name="q" type="text" value="" title="<?php _e('Search by Keyword', 'beneteau'); ?>"/>
@@ -44,13 +44,14 @@ get_header(); ?>
                     <input id="keywordsearch-locationsearch" name="locationsearch" type="text" value="" title="<?php _e('Search by Location', 'beneteau'); ?>"/> <input class="keywordsearch-source" type="hidden" name="utm_source" value="CSSearchWidget" /> 
                 </div>
                 <input class="keywordsearch-startrow" type="hidden" name="startrow" value="1" /> 
+                <input class="keywordsearch-startrow" type="hidden" name="locale" value="<?php _e('en_US', 'beneteau'); ?>" /> 
                 <button id="keywordsearch-button" type="submit"><?php _e('Search', 'beneteau'); ?></button> 
             </form>
             <script src="https://code.jquery.com/jquery-1.7.2.min.js"></script> 
-            <script src="https://beneteau-group.jobs2web.com/view/widgets/keywordsearchHandler.js"></script>
+            <script src="https://jobs.beneteau-group.com/view/widgets/keywordsearchHandler.js"></script>
 
 
-            <iframe src='https://rmk-map-2.valhalla2.stage.jobs2web.com/map/?esid=HI2MHAJYg9DEYIQhTPsdSw%3D%3D&locale=en_US&uselcl=false&watercolor=%23FFFFFF&jobdomain=beneteau.valhalla2.stage.jobs2web.com &maplbljob=Job&maplbljobs=jobs&mapbtnsearchjobs=Search+jobs&centerpoint=35.1,31.3&mapzoom=1&keyword=&regionCode=US&parentURL=http%3A%2F%2F beneteau.valhalla2.stage.jobs2web.com%2Fcontent%2FSearch-by-Location%2F%3Flocale%3Dfr_FR' class='sf-map' frameborder='0' style='width:100%;height:400px;'></iframe>
+            <iframe src='https://rmk-map-2.jobs2web.com/map/?esid=qddXYpVti7hf0TpJAITamA%3D%3D&locale=<?php _e('en_US', 'beneteau'); ?>&uselcl=false&watercolor=%23FFFFFF&jobdomain=beneteau-group.jobs2web.com&maplbljob=Job&maplbljobs=jobs&mapbtnsearchjobs=Search+jobs&centerpoint=25,5&mapzoom=1&keyword=&regionCode=US&parentURL=http%3A%2F%2Fbeneteau-group.jobs2web.com%2F' class='sf-map' frameborder='0' style='width:100%;height:400px;'></iframe>
 
             <?php
                 
